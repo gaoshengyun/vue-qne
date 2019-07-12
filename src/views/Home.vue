177a690af52bc3771c295515e88f4038707e124e
gaoshengyun <18616242909@163.com>
2019-07-12 16:26:40 +0800

    home-header组件

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
-    hello world
+    <home-header></home-header>
   </div>
 </template>
 
 <script>
-
-
+import HomeHeader from '../components/home/Header.vue'
 export default {
   name: 'home',
+  components:{
+    HomeHeader
+  }
 }
 </script>
