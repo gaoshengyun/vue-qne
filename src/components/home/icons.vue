b5cc4ed60cb48fbb239d83c322692ccd98b09755
gaoshengyun <18616242909@163.com>
2019-07-12 18:48:32 +0800

    index-icon

---
+++ src/components/home/icons.vue
@@ -0,0 +1,131 @@
+<template>
+  <div class="icons">
+    <swiper>
+      <swiper-slide v-for="(page, index) in pages" :key="index">
+        <div class="icon" v-for="(item, index) in page" :key="item.id">
+          <div class="icon-img">
+            <img class="icon-img-content" :src="item.imgUrl" alt="">
+          </div>
+          <p class="icon-desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+
+      
+    </swiper>
+  </div>
+</template>
+<script>
+export default {
+  name:'home-icons',
+  data(){
+    return {
+      iconList:[
+        {
+          id:'0001',
+          imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          desc:'景点门票'
+        },
+        {
+          id:'0002',
+          imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/3e/86314b2af03b7502.png',
+          desc:'水上乐园水上乐园水上乐园'
+        },
+        {
+          id:'0003',
+          imgUrl:'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20194/3ef092d0a89ffddb7f781cda30c8ae49.png',
+          desc:'暑假大促'
+        },
+        {
+          id:'0004',
+          imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/f5/c4c9d9830404e602.png',
+          desc:'室内娱乐'
+        },
+        {
+          id:'0005',
+          imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png',
+          desc:'动植物园'
+        },
+        {
+          id:'0006',
+          imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/95/8246f27355943202.png',
+          desc:'世纪公园'
+        },
+        {
+          id:'0007',
+          imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/47/c2b659e048b11602.png',
+          desc:'上海野生'
+        },
+        {
+          id:'0008',
+          imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/de/f26be47a6bf02a02.png',
+          desc:'上海迪士尼'
+        },
+        {
+          id:'0009',
+          imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/3e/86314b2af03b7502.png',
+          desc:'清凉避暑'
+        },
+      ]
+    }
+  },
+  computed: {
+    pages(){
+      const pages = []
+      this.iconList.forEach((item,index) => {
+        const page = Math.floor(index / 8)
+        if(!pages[page]){
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  },
+}
+</script>
+<style lang="less" scoped>
+  @import "../../assets/styles/vaiables.less";
+  @import "../../assets/styles/mixin.less";
+  .swiper-container{
+    height: 0;
+    padding-bottom: 50%;
+  }
+  .icons{
+    height: 0;
+    padding-bottom: 50%;
+    overflow: hidden;
+    .icon{
+      width: 25%;
+      height: 0;
+      float: left;
+      padding-bottom: 25%;
+      overflow: hidden;
+      position: relative;
+      .icon-img{
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0.44rem;
+        box-sizing: border-box;
+        padding: .1rem;
+        .icon-img-content{
+          height: 100%;
+          display: block;
+          margin: 0 auto;
+        }
+      }
+      .icon-desc{
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        height: .44rem;
+        line-height: .44rem;
+        color:@darkTextColor;
+        text-align: center;
+        .ellipsos()
+      }
+    }
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div>
     <home-header></home-header>
     <home-swiper></home-swiper>
+    <home-icons></home-icons>
   </div>
 </template>
 
 <script>
 import HomeHeader from '../components/home/Header.vue'
 import HomeSwiper from '../components/home/Swiper.vue'
+import HomeIcons from '../components/home/icons.vue'
 export default {
   name: 'home',
   components:{
     HomeHeader,
-    HomeSwiper
+    HomeSwiper,
+    HomeIcons
   }
 }
 </script>
