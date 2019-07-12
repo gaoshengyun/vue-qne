0333091ccb109be7f95e89fc6dbef1cd7e0a06a8
gaoshengyun <18616242909@163.com>
2019-07-12 17:48:28 +0800

    swiper

--- src/main.js
+++ src/main.js
@@ -6,9 +6,12 @@ import fastclick from 'fastclick'
 import './assets/styles/reset.css'
 import './assets/styles/border.css'
 import './assets/font/iconfont.css'
-
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.min.css'
 Vue.config.productionTip = false
 fastclick.attach(document.body)
+
+Vue.use(VueAwesomeSwiper)
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <home-header></home-header>
+    <home-swiper></home-swiper>
   </div>
 </template>
 
 <script>
 import HomeHeader from '../components/home/Header.vue'
+import HomeSwiper from '../components/home/Swiper.vue'
 export default {
   name: 'home',
   components:{
-    HomeHeader
+    HomeHeader,
+    HomeSwiper
   }
 }
 </script>
