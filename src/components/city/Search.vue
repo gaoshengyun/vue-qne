70d5d3a0056ca973eb341739e6b126b53993b8b8
gaoshengyun <18616242909@163.com>
2019-07-13 17:16:02 +0800

    城市搜索框

---
+++ src/components/city/Search.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="search">
+    <input class="search-input" type="text" placeholder="输入城市名或拼音">
+  </div>
+</template>
+<script>
+export default {
+  name:'CitySearch'
+}
+</script>
+<style lang="less" scoped>
+  @import '../../assets/styles/vaiables.less';
+  .search{
+    height: 0.72rem;
+    background: @bgColor;
+    padding: 0 .1rem;
+    .search-input{
+      height: 0.63rem;
+      line-height: 0.62rem;
+      width: 100%;
+      text-align: center;
+      border-radius: .06rem;
+      color: #666;
+      padding: 0 .1rem;
+      box-sizing: border-box;
+    }
+  }
+</style>
--- src/views/City.vue
+++ src/views/City.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
     <city-header></city-header>
+    <city-search></city-search>
   </div>
 </template>
 <script>
 import CityHeader from '../components/city/Header.vue'
+import CitySearch from '../components/city/Search.vue'
 export default {
   name:'city',
   components:{
-    CityHeader
+    CityHeader,
+    CitySearch
   }
 }
 </script>
