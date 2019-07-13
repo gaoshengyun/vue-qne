0e8a9237055dddfd9ea71069859945e90e3d0cc7
gaoshengyun <18616242909@163.com>
2019-07-13 21:24:35 +0800

    城市选择静态页

---
+++ src/components/city/Alphabet.vue
@@ -0,0 +1,43 @@
+<template>
+  <ul class="list">
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+  </ul>
+</template>
+<script>
+export default {
+  name:'CityAlphabet'
+}
+</script>
+<style lang="less" scoped>
+  @import '../../assets/styles/vaiables.less';
+  .list{
+    position: absolute;
+    right: 0;
+    top: 1.58rem;
+    bottom: 0;
+    width: 0.4rem;
+    color: @bgColor;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    .item{
+      text-align: center;
+      line-height: 0.4rem;
+    }
+  }
+  
+</style>
--- src/views/City.vue
+++ src/views/City.vue
@@ -2,16 +2,22 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
+    <city-list></city-list>
+    <city-alphabet></city-alphabet>
   </div>
 </template>
 <script>
 import CityHeader from '../components/city/Header.vue'
 import CitySearch from '../components/city/Search.vue'
+import CityList from '../components/city/List.vue'
+import CityAlphabet from '../components/city/Alphabet.vue'
 export default {
   name:'city',
   components:{
     CityHeader,
-    CitySearch
+    CitySearch,
+    CityList,
+    CityAlphabet
   }
 }
 </script>
