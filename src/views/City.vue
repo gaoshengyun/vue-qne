16bed9ca61a91be7be406c02f41fdd5fc989c331
gaoshengyun <18616242909@163.com>
2019-07-13 22:17:23 +0800

    ajax获取城市页面数据

--- src/views/City.vue
+++ src/views/City.vue
@@ -2,11 +2,12 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
-    <city-list></city-list>
-    <city-alphabet></city-alphabet>
+    <city-list :cities="cities" :hot="hotCities"></city-list>
+    <city-alphabet :cities="cities"></city-alphabet>
   </div>
 </template>
 <script>
+import axios from 'axios'
 import CityHeader from '../components/city/Header.vue'
 import CitySearch from '../components/city/Search.vue'
 import CityList from '../components/city/List.vue'
@@ -18,7 +19,30 @@ export default {
     CitySearch,
     CityList,
     CityAlphabet
-  }
+  },
+  data() {
+    return {
+      cities:{},
+      hotCities:[]
+    }
+  },
+  mounted() {
+    this.getCityInfo()
+  },
+  methods: {
+    getCityInfo(){
+      axios.get('/api/city.json')
+        .then(this.handleGetCityInfoSucc)
+    },
+    handleGetCityInfoSucc(res){
+      res = res.data
+      if(res.ret && res.data){
+        const data = res.data
+        this.cities = data.cities
+        this.hotCities = data.hotCities
+      }
+    }
+  },
 }
 </script>
 <style lang="less" scoped>
